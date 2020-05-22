21b3ad88272e9119cd970db773ca5a9a632f1b9f
Twixes <dev@twixes.com>
2020-05-22 23:58:35 +0200

    Improve code style

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,10 @@
-import * as express from "express"
-import * as logger from "morgan"
+import * as express from 'express'
+import * as morgan from 'morgan'
 import * as indexRouter from './routes/index'
 
 const app: express.Application = express()
 
-app.use(logger('dev'))
+app.use(morgan('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use('/', indexRouter)
