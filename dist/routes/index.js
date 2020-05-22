1aa2e56dd49121feb16733bb17084e10cddf43de
Twixes <dev@twixes.com>
2020-05-23 00:00:57 +0200

    Remove compiled files

--- dist/routes/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-const express = require("express");
-const router = express.Router();
-router.get('/', function (_, res) {
-    res.json({});
-});
-module.exports = router;
-//# sourceMappingURL=index.js.map
