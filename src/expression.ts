8fedace04798f09292223a872e263572b11e67aa
Twixes <dev@twixes.com>
2020-05-23 03:45:26 +0200

    Add expression tests and implementation

---
+++ src/expression.ts
@@ -0,0 +1,58 @@
+type PowerCoefficientPair = [number, number]
+
+
+export class Expression {
+    coefficientToPower: Map<number, number> = new Map()
+
+    constructor(input?: PowerCoefficientPair[] | Map<number, number>) {
+        if (input) {
+            if (Array.isArray(input)) this.coefficientToPower = new Map(input)
+            else this.coefficientToPower = input
+        }
+    }
+
+    toString(): string {
+        const powerCoefficientPairs: PowerCoefficientPair[] = Array.from(this.coefficientToPower.entries()).sort(
+            (a: PowerCoefficientPair, b: PowerCoefficientPair) => b[0] - a[0]
+        )
+        let stringSoFar = ''
+        for (const [power, coefficient] of powerCoefficientPairs) {
+            if (coefficient === 0) continue
+            if (stringSoFar) stringSoFar += coefficient > 0 ? ' + ' : ' - '
+            stringSoFar += [
+                coefficient !== 1 ? `${Math.abs(coefficient)}` : '',
+                coefficient !== 1 && power !== 0 ? '*' : '',
+                power !== 0 ? 'x' : '',
+                power !== 1 && power !== 0 ? (power < 0 ? `^(${power})` : `^${power}`) : ''
+            ].join('')
+        }
+        powerCoefficientPairs.filter(([, coefficient]) => coefficient !== 0).map(
+            ([power, coefficient]) => [
+                coefficient !== 1 ? `${coefficient}` : '',
+                coefficient !== 1 && power !== 0 ? '*' : '',
+                power !== 0 ? 'x' : '',
+                power !== 1 && power !== 0 ? `^${power}` : ''
+            ].join('')
+        )
+        return stringSoFar
+    }
+
+    calculateFor(x: number): number {
+        let y = 0
+        for (const [power, coefficient] of this.coefficientToPower.entries()) if (coefficient) {
+            y += coefficient * x**power
+        }
+        return y
+    }
+
+     // shame there's no operator redefinition in JS, anyway: opted for a functional approach here instead of in-place
+    static sum(...summands: Expression[]): Expression {
+        const sum: Expression = new Expression()
+        for (const summand of summands) {
+            for (const [power, coefficient] of summand.coefficientToPower.entries()) {
+                sum.coefficientToPower.set(power, (sum.coefficientToPower.get(power) || 0) + coefficient)
+            }
+        }
+        return sum
+    }
+}
---
+++ src/tests/expression.test.ts
@@ -0,0 +1,59 @@
+import * as assert from 'assert'
+import { Expression } from '../expression'
+
+describe('Expression', () => {
+    describe('#constructor()', () => {
+        it('should work without input by constructing an empty, falsy instance', () => {
+            const expression: Expression = new Expression()
+            assert.equal(expression, false)
+            assert.deepStrictEqual(expression.coefficientToPower, new Map())
+        })
+        it('should work with Map<number, number> input', () => {
+            const map: Map<number, number> = new Map([[9, 6], [3, 0]])
+            const expression: Expression = new Expression(map)
+            assert.deepStrictEqual(expression.coefficientToPower, map)
+        })
+        it('should work with Array<number> input', () => {
+            const array: [number, number][] = [[10000000, -10000000], [0, 0]]
+            const expression: Expression = new Expression(array)
+            assert.deepStrictEqual(expression.coefficientToPower, new Map(array))
+        })
+    })
+    describe('#toString()', () => {
+        it('should handle all positive powers and coefficients in descending order of powers', () => {
+            const expression: Expression = new Expression([[1,1], [666, 3], [2, 3]])
+            assert.strictEqual(expression.toString(), '3*x^666 + 3*x^2 + x')
+        })
+        it('should handle a zero power', () => {
+            const expression: Expression = new Expression([[2, 2137], [0, 1337]])
+            assert.strictEqual(expression.toString(), '2137*x^2 + 1337')
+        })
+        it('should handle a negative power', () => {
+            const expression: Expression = new Expression([[7, 9], [-5, 32]])
+            assert.strictEqual(expression.toString(), '9*x^7 + 32*x^(-5)')
+        })
+        it('should ignore a zero coefficient', () => {
+            const expression: Expression = new Expression([[7, 9], [42, 0]])
+            assert.strictEqual(expression.toString(), '9*x^7')
+        })
+        it('should handle a negative coefficient', () => {
+            const expression: Expression = new Expression([[7, 9], [5, -32]])
+            assert.strictEqual(expression.toString(), '9*x^7 - 32*x^5')
+        })
+    })
+    describe('#calculateFor()', () => {
+        it('should return the correct result assuming x', () => {
+            const expression: Expression = new Expression([[2, 2137], [-0, -1337], [-4, -4]])
+            assert.strictEqual(expression.calculateFor(997), 2137 * 997**2 - 1337 - 4 * 997**(-4))
+        })
+    })
+    describe('#sum()', () => {
+        it('should return the sum of given expressions', () => {
+            const expression1: Expression = new Expression([[2, 2], [0, 3]])
+            const expression2: Expression = new Expression([[2, 1], [3, 3]])
+            const expression3: Expression = new Expression()
+            const expressionSum: Expression = Expression.sum(expression1, expression2, expression3)
+            assert.strictEqual(expressionSum.toString(), '3*x^3 + 3*x^2 + 3')
+        })
+    })
+})
