ad5980a5cfd0dcf3c366ccf7070668ec96ba5541
Twixes <dev@twixes.com>
2020-05-25 23:37:23 +0200

    Optimize CLI

--- src/cli.ts
+++ src/cli.ts
@@ -4,7 +4,7 @@ import { Expression, ExpressionRaw } from './models/expression'
 const options: string[] = ['SUM', 'CALCULATE']
 let optionIndex = -1
 while (true) {
-    optionIndex = readlineSync.keyInSelect(['SUM', 'CALCULATE'], 'What do you want to do?')
+    optionIndex = readlineSync.keyInSelect(options, 'What do you want to do?')
     if (options[optionIndex] === 'SUM') {
         const expressionsRawInput: string = readlineSync.question(
             'Provide array of raw expressions (e.g. [[[2, 2], [0, 3]], [[3, 3], [2, 1]]]): '
@@ -15,8 +15,8 @@ while (true) {
             const sum: Expression = Expression.sum(...expressionsRaw.map(
                 expressionRawCandidate => new Expression(expressionRawCandidate)
             ))
-            console.log(sum.toPowerCoefficientPairs())
-            console.log(sum.toString())
+            console.log(`Result pairs: ${JSON.stringify(sum.toPowerCoefficientPairs())}`)
+            console.log(`Result string: ${sum}`)
         } catch (err) {
             console.log(err.toString())
             continue
@@ -37,7 +37,7 @@ while (true) {
         const variableValue: number = readlineSync.questionFloat('Provide variable x value (e.g. 4): ')
         try {
             const result: number = expression.calculateFor(variableValue)
-            console.log(result)
+            console.log(`Result: ${result}`)
         } catch (err) {
             console.log(err.toString())
             continue
